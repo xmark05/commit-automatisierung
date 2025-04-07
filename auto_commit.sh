69253a6573242b4fa03bdf7be3e8cb6e4fc26ce9
xmark05 <180277366+xmark05@users.noreply.github.com>
2025-04-07 10:43:17 +0200

    Automatischer Commit #1 - 2025-04-07 10:43:17

--- auto_commit.sh
+++ auto_commit.sh
@@ -4,18 +4,20 @@
 REPO_PATH="/home/mark/Dokumente/GitHub/commit-automatisierung"
 cd $REPO_PATH  # Wechselt in das Repository
 
-# Git-Befehle für den Commit
-git add .  # Fügt alle Änderungen hinzu
-
-# Führe 3 Commits pro Tag aus, z. B. alle 8 Stunden
 for i in {1..3}
 do
-  COMMIT_MESSAGE="Automatischer Commit #$i - $(date +'%Y-%m-%d %H:%M:%S')"
-  git commit -m "$COMMIT_MESSAGE"  # Commit mit der Nachricht
-  git push origin main  # Push zu GitHub
-  
-  # Warte 8 Stunden (28800 Sekunden)
+  # Checke ob es Änderungen gibt
+  if [[ -n $(git status --porcelain) ]]; then
+    git add .
+    COMMIT_MESSAGE="Automatischer Commit #$i - $(date +'%Y-%m-%d %H:%M:%S')"
+    git commit -m "$COMMIT_MESSAGE"
+    git push origin main
+  else
+    echo "[$(date)] Keine Änderungen zum Committen."
+  fi
+
+  # Warten, außer beim letzten Lauf
   if [ $i -lt 3 ]; then
-    sleep 28800  # Warten für 8 Stunden
+    sleep 28800  # 8 Stunden
   fi
 done
